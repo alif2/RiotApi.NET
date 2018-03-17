6f554b312041273c527fbb3b8c4fbc5aa4247411
Adam Lif <adamlnk@neb.rr.com>
2018-03-17 17:35:01 -0400

    Refactor out OptionalParameters.cs

--- RiotApi.NET/MatchApi.cs
+++ RiotApi.NET/MatchApi.cs
@@ -1,74 +1,40 @@
 ﻿using RiotApi.NET.Objects;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
 
 namespace RiotApi.NET
 {
-    public class MatchApi
+    public class MatchApi : RiotApi
     {
-        private static HttpClient _httpClient = new HttpClient { BaseAddress = new Uri("https://na1.api.riotgames.com") };
-        private static string _apiKey = "RGAPI-9aeb4819-3ba4-4268-87b2-e0568333a4e6";
         private static string _baseUrl = "/lol/match/v3";
 
         public static Match GetMatchById(long matchId)
         {
-            var httpResponseMessage = _httpClient.GetAsync(_baseUrl + $"/matches/{matchId}?api_key={_apiKey}").Result;
-            httpResponseMessage.EnsureSuccessStatusCode();
-
-            return httpResponseMessage.Content.ReadAsAsync<Match>().Result;
+            return GetObject<Match>(_baseUrl + $"/matches/{matchId}");
         }
 
-        public static MatchList GetMatchesByAccountId(int accountId, IEnumerable<int> seasons = null, IEnumerable<int> queueIds = null, 
-            IEnumerable<int> championIds = null, int beginIndex = -1, int endIndex = -1, long beginTime = -1, long endTime = -1)
+        public static MatchList GetMatchesByAccountId(int accountId, OptionalParameters optionalParameters)
         {
-            var optionalParameters = string.Empty;
-
-            if (seasons != null) optionalParameters += "&season=" + string.Join(",", seasons.ToArray());
-            if (queueIds != null) optionalParameters += "&queue=" + string.Join(",", queueIds.ToArray());
-            if (championIds != null) optionalParameters += "&champion=" + string.Join(",", championIds.ToArray());
-            if (beginIndex != -1) optionalParameters += "&beginIndex=" + beginIndex;
-            if (endIndex != -1) optionalParameters += "&endIndex=" + endIndex;
-            if (beginTime != -1) optionalParameters += "&beginTime=" + beginTime;
-            if (endTime != -1) optionalParameters += "&endTime=" + endTime;
-
-            var httpResponseMessage = _httpClient.GetAsync(_baseUrl + $"/matchlists/by-account/{accountId}?api_key={_apiKey}{optionalParameters}").Result;
-            httpResponseMessage.EnsureSuccessStatusCode();
-
-            return httpResponseMessage.Content.ReadAsAsync<MatchList>().Result;
+            return GetObjectWithOptionalParameters<MatchList>(_baseUrl + $"/matchlists/by-account/{accountId}", optionalParameters);
         }
 
         public static MatchList GetRecentMatchesByAccountId(int accountId)
         {
-            var httpResponseMessage = _httpClient.GetAsync(_baseUrl + $"/matchlists/by-account/{accountId}/recent?api_key={_apiKey}").Result;
-            httpResponseMessage.EnsureSuccessStatusCode();
-
-            return httpResponseMessage.Content.ReadAsAsync<MatchList>().Result;
+            return GetObject<MatchList>(_baseUrl + $"/matchlists/by-account/{accountId}/recent");
         }
 
         public static MatchTimeline GetMatchTimelineById(long matchId)
         {
-            var httpResponseMessage = _httpClient.GetAsync(_baseUrl + $"/timelines/by-match/{matchId}?api_key={_apiKey}").Result;
-            httpResponseMessage.EnsureSuccessStatusCode();
-
-            return httpResponseMessage.Content.ReadAsAsync<MatchTimeline>().Result;
+            return GetObject<MatchTimeline>(_baseUrl + $"/timelines/by-match/{matchId}");
         }
 
         public static IEnumerable<long> GetMatchesByTournamentCode(string tournamentCode)
         {
-            var httpResponseMessage = _httpClient.GetAsync(_baseUrl + $"/matches/by-tournament-code/{tournamentCode}/ids?api_key={_apiKey}").Result;
-            httpResponseMessage.EnsureSuccessStatusCode();
-
-            return httpResponseMessage.Content.ReadAsAsync<IEnumerable<long>>().Result;
+            return GetObject<IEnumerable<long>>(_baseUrl + $"/matches/by-tournament-code/{tournamentCode}/ids");
         }
 
         public static Match GetMatchByTournamentCode(long matchId, string tournamentCode)
         {
-            var httpResponseMessage = _httpClient.GetAsync(_baseUrl + $"/matches/{matchId}/by-tournament-code/{tournamentCode}?api_key={_apiKey}").Result;
-            httpResponseMessage.EnsureSuccessStatusCode();
-
-            return httpResponseMessage.Content.ReadAsAsync<Match>().Result;
+            return GetObject<Match>(_baseUrl + $"/matches/{matchId}/by-tournament-code/{tournamentCode}");
         }
     }
 }
---
+++ RiotApi.NET/Objects/OptionalParameters.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RiotApi.NET.Objects
+{
+    public class OptionalParameters
+    {
+        public IEnumerable<int> Seasons { get; set; }
+        public IEnumerable<int> QueueIds { get; set; }
+        public IEnumerable<int> ChampionIds { get; set; }
+
+        public int BeginIndex { get; set; }
+        public int EndIndex { get; set; }
+        public long BeginTime { get; set; }
+        public long EndTime { get; set; }
+
+        public OptionalParameters(IEnumerable<int> seasons = null, IEnumerable<int> queueIds = null,
+            IEnumerable<int> championIds = null, int beginIndex = -1, int endIndex = -1, long beginTime = -1, long endTime = -1)
+        {
+            Seasons = seasons;
+            QueueIds = queueIds;
+            ChampionIds = championIds;
+            BeginIndex = beginIndex;
+            EndIndex = endIndex;
+            BeginTime = beginTime;
+            EndTime = endTime;
+        }
+
+        public override string ToString()
+        {
+            var optionalParameters = string.Empty;
+
+            if (Seasons != null) optionalParameters += "&season=" + string.Join(",", Seasons.ToArray());
+            if (QueueIds != null) optionalParameters += "&queue=" + string.Join(",", QueueIds.ToArray());
+            if (ChampionIds != null) optionalParameters += "&champion=" + string.Join(",", ChampionIds.ToArray());
+            if (BeginIndex != -1) optionalParameters += "&beginIndex=" + BeginIndex;
+            if (EndIndex != -1) optionalParameters += "&endIndex=" + EndIndex;
+            if (BeginTime != -1) optionalParameters += "&beginTime=" + BeginTime;
+            if (EndTime != -1) optionalParameters += "&endTime=" + EndTime;
+
+            return optionalParameters;
+        }
+    }
+}
